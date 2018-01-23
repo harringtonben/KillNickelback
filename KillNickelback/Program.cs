e85de0190f56d3b249ff367dd107091487d1ccb1
Ben Harrington <harringtonben@icloud.com>
2018-01-22 18:14:55 -0600

    finished creating hashset

--- KillNickelback/Program.cs
+++ KillNickelback/Program.cs
@@ -10,6 +10,17 @@ namespace KillNickelback
     {
         static void Main(string[] args)
         {
+            var goodSongs = new List<string>();
+            var allSongs = new HashSet<Song>();
+
+            allSongs.Add(new Song { Artist = "Nickelback", Name = "Photograph" });
+            allSongs.Add(new Song { Artist = "Tom Petty", Name = "Free Fallin'" });
+            allSongs.Add(new Song { Artist = "Nickelback", Name = "How You Remind Me" });
+            allSongs.Add(new Song { Artist = "Nickelback", Name = "Someday" });
+            allSongs.Add(new Song { Artist = "Bruce Springsteen", Name = "Born To Run" });
+            allSongs.Add(new Song { Artist = "Graham Nash", Name = "Military Madness" });
+            allSongs.Add(new Song { Artist = "Neil Young", Name = "Heart of Gold" });
+
         }
     }
 }
